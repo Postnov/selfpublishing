15679026aafcf1661aa05e55b086a6bedd482bdc
Daniil Postnov <dpostnov@ronasit.com>
2019-07-23 14:12:02 +0300

    finish top-section

---
+++ src/js/partials/common.js
@@ -0,0 +1,10 @@
+var topSlider = new Swiper ('.js-top-slider', {
+  init: true,
+  effect: 'fade',
+  nested: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
