a607e7f1289fddc2c97d4eecfec046ae48772ba8
Daniil Postnov <dpostnov@ronasit.com>
2019-07-24 17:55:27 +0300

    add dist

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,3 +10,27 @@ var topSlider = new Swiper('.js-top-slider', {
     clickable: true
   }
 });
+var publishSlider = new Swiper('.js-publish-slider', {
+  init: true,
+  nested: true,
+  slidesPerView: 4,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    650: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    979: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
+});
