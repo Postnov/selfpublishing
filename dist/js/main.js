e6957bdd9778dac84acde8ccd02b6c6f301c125b
Daniil Postnov <dpostnov@ronasit.com>
2019-07-25 08:05:40 +0300

    add dist

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,74 @@
 "use strict";
 
+// function 'closest'
+!function (e) {
+  "function" != typeof e.matches && (e.matches = e.msMatchesSelector || e.mozMatchesSelector || e.webkitMatchesSelector || function (e) {
+    for (var t = this, o = (t.document || t.ownerDocument).querySelectorAll(e), n = 0; o[n] && o[n] !== t;) {
+      ++n;
+    }
+
+    return Boolean(o[n]);
+  }), "function" != typeof e.closest && (e.closest = function (e) {
+    for (var t = this; t && 1 === t.nodeType;) {
+      if (t.matches(e)) return t;
+      t = t.parentNode;
+    }
+
+    return null;
+  });
+}(window.Element.prototype);
+var modalButtons = document.querySelectorAll('.js-open-modal'),
+    overlay = document.querySelector('.js-overlay-modal'),
+    closeButtons = document.querySelectorAll('.js-close-modal'),
+    body = document.querySelector('body');
+modalButtons.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    var modalId = this.getAttribute('data-modal'),
+        modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+    modalElem.classList.add('active');
+    overlay.classList.add('active');
+    body.classList.add('no-scroll');
+  }); // end click
+}); // end foreach
+
+closeButtons.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    var parentModal = this.closest('.modal');
+    parentModal.classList.remove('active');
+    overlay.classList.remove('active');
+    body.classList.remove('no-scroll');
+  });
+}); // end foreach
+
+document.body.addEventListener('keyup', function (e) {
+  var key = e.keyCode;
+
+  if (key == 27) {
+    document.querySelector('.modal.active').classList.remove('active');
+    document.querySelector('.overlay').classList.remove('active');
+    body.classList.remove('no-scroll');
+  }
+
+  ;
+}, false);
+overlay.addEventListener('click', function () {
+  document.querySelector('.modal.active').classList.remove('active');
+  this.classList.remove('active');
+  body.classList.remove('no-scroll');
+});
+document.querySelectorAll('.js-switch-modal').forEach(function (item) {
+  var event = item.getAttribute('data-event');
+  item.addEventListener(event, function (e) {
+    e.preventDefault();
+    var currentModal = this.closest('.modal').getAttribute('data-modal'),
+        currentModalEl = document.querySelector('.modal[data-modal="' + currentModal + '"]'),
+        nextModal = this.getAttribute('data-next-modal'),
+        nextModalEl = document.querySelector('.modal[data-modal="' + nextModal + '"]');
+    currentModalEl.classList.remove('active');
+    nextModalEl.classList.add('active');
+  });
+});
 var topSlider = new Swiper('.js-top-slider', {
   init: true,
   effect: 'fade',
